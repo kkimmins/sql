99baae6fd2dc79e7bb7c7510b81465e57c455b27
Kenny Kimmins <kimmins.kenneth@gmail.com>
2026-04-01 03:28:30 -0400

    assignment 1 completed

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -6,20 +6,18 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 --QUERY 1
-
-
-
-
+SELECT * 
+FROM customer;
 --END QUERY
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the customer table, 
 sorted by customer_last_name, then customer_first_ name. */
 --QUERY 2
-
-
-
-
+SELECT * 
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
 --END QUERY
 
 
@@ -27,10 +25,10 @@ sorted by customer_last_name, then customer_first_ name. */
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. 
 Limit to 25 rows of output. */
 --QUERY 3
-
-
-
-
+SELECT * 
+FROM customer_purchases
+WHERE product_id IN (4,9)
+LIMIT 25;
 --END QUERY
 
 
@@ -42,10 +40,10 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 Limit to 25 rows of output.
 */
 --QUERY 4
-
-
-
-
+SELECT *, quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE customer_id BETWEEN 8 AND 10
+LIMIT 25;
 --END QUERY
 
 
@@ -55,10 +53,14 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 --QUERY 5
+SELECT product_id, product_name
 
-
-
-
+, CASE WHEN product_qty_type = 'unit' 
+			THEN 'unit'
+			ELSE 'bulk'
+			END as prod_qty_type_condensed
+			
+FROM product;
 --END QUERY
 
 
@@ -66,10 +68,19 @@ if the product_qty_type is “unit,” and otherwise displays the word “bulk.�
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 --QUERY 6
+SELECT product_id, product_name
 
+, CASE WHEN product_qty_type = 'unit' 
+			THEN 'unit'
+			ELSE 'bulk'
+			END as prod_qty_type_condensed
 
+, CASE WHEN product_name LIKE  '%pepper%' 
+			THEN 1
+			ELSE 0
+			END as pepper_flag
 
-
+FROM product;
 --END QUERY
 
 
@@ -78,10 +89,17 @@ contains the word “pepper” (regardless of capitalization), and otherwise out
 vendor_id field they both have in common, and sorts the result by market_date, then vendor_name.
 Limit to 24 rows of output. */
 --QUERY 7
+SELECT market_date,
+vendor_name,
+vendor.vendor_id,
+vendor_booth_assignments.vendor_id
 
+FROM vendor
+INNER JOIN vendor_booth_assignments
+	ON vendor.vendor_id = vendor_booth_assignments.vendor_id
 
-
-
+ORDER BY market_date, vendor_name
+LIMIT 24;
 --END QUERY
 
 
@@ -92,10 +110,9 @@ Limit to 24 rows of output. */
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 --QUERY 8
-
-
-
-
+SELECT vendor_id, COUNT(booth_number) as num_of_booths 
+FROM vendor_booth_assignments 
+GROUP BY vendor_id;
 --END QUERY
 
 
@@ -105,10 +122,19 @@ of customers for them to give stickers to, sorted by last name, then first name.
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 --QUERY 9
+SELECT customer_last_name, 
+customer_first_name, 
+customer.customer_id, 
+customer_purchases.customer_id, 
+SUM(quantity * cost_to_customer_per_qty) AS total_spend 
 
-
-
-
+FROM customer 
+INNER JOIN customer_purchases 
+	ON customer.customer_id = customer_purchases.customer_id 
+	
+GROUP BY customer.customer_id
+HAVING total_spend > 2000
+ORDER BY customer_last_name, customer_first_name;
 --END QUERY
 
 
@@ -124,10 +150,14 @@ When inserting the new vendor, you need to appropriately align the columns to be
 VALUES(col1,col2,col3,col4,col5) 
 */
 --QUERY 10
+DROP TABLE IF EXISTS temp.new_vendor;
 
+CREATE TABLE temp.new_vendor AS
+SELECT *
+FROM vendor;
 
-
-
+INSERT INTO temp.new_vendor
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 --END QUERY
 
 
